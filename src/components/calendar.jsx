7c1818bf4492a6015f552d5ad4b5f095bdc051fb
Cameron Icso <cameron.icso@cimpress.com>
2019-06-04 10:45:18 -0400

    add calendar and start slideshow

--- src/components/calendar.jsx
+++ src/components/calendar.jsx
@@ -4,7 +4,7 @@ export default class Calendar extends Component {
   render() {
     return(
       <div>
-        You're at the Calendar section.
+        <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FNew_York&amp;src=aWNzb2MyMkBnbWFpbC5jb20&amp;color=%237986CB" style={{"borderWidth":0}} width="800" height="600" frameBorder="0" scrolling="no"></iframe>
       </div>
     )
   }
--- src/components/photos.jsx
+++ src/components/photos.jsx
@@ -1,10 +1,38 @@
 import React, { Component } from 'react'
+import { Slide } from 'react-slideshow-image';
+const properties = {
+  duration: 5000,
+  transitionDuration: 500,
+  infinite: true,
+  indicators: true,
+  arrows: true
+}
+
+let images = ['../assets/images/slideshow/image1.jpg', '../assets/images/slideshow/image2.jpg']
 
 export default class Photos extends Component {
+  constructor(props) {
+    super(props)
+    this.generateSlides = this.generateSlides.bind(this)
+  }
+
+  generateSlides() {
+    return images.map(image => {
+      return(
+        <div className="each-slide">
+          <div style={{'backgroundImage': `url(${image})`}}>
+            <span>Slide</span>
+          </div>
+        </div>
+      )
+    })
+  }
   render() {
     return(
       <div>
-        You're at the photos section.
+        <Slide {...properties}>
+          {this.generateSlides()}
+        </Slide>
       </div>
     )
   }
